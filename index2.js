cdad811b3592a73760f45dc0c70b9491a50c9a9c
Edgar120008 <edgarenano1208@outlook.com>
2024-10-03 15:12:53 -0600

    first commit

--- index2.js
+++ index2.js
@@ -26,6 +26,8 @@ app.get('/generate-pdf', (req, res) => {
     const consumo = '1';
     const uso = 'Domestico H';
     const promedio = '1';
+    let index = 0; 
+    let total = 0;
     const importe = [
         {
             concepto: "consumo",
@@ -167,11 +169,314 @@ app.get('/generate-pdf', (req, res) => {
     doc.text('Importe facturado', 40, 146);
     doc.setFontSize(13);
     doc.setFont("Helvetica", "");
-    doc.text('Ultimos pagos', 150, 146);
+    doc.text('Ultimos pagos', 148, 146);
 
     doc.setLineWidth(1);
     doc.setDrawColor('#5e5e5e');
     doc.line(5, 150, 205, 150);
+
+    doc.setLineWidth(1);
+    doc.setDrawColor('#5e5e5e');
+    doc.line(110, 139, 110, 230);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Concepto', 10, 156);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Mes', 50, 156);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Vencido', 80, 156);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Fecha', 120, 156);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Recibo', 150, 156);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Imp. Pagado', 180, 156);
+
+// 
+// 
+    doc.setLineWidth(1);
+    doc.setDrawColor('#5e5e5e');
+    doc.line(5, 230, 205, 230);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Datos de facturacion', 87, 236);
+
+    doc.setLineWidth(1);
+    doc.setDrawColor('#5e5e5e');
+    doc.line(5, 240, 205, 240);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Periodo facturado', 90, 247);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Fecha Vencimiento:', 40, 253);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('INMEDIATO', 150, 253);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Meses vencidos:', 40, 260);
+    doc.setFontSize(13);
+    // doc.setFont("Helvetica", "");
+    // doc.text('INMEDIATO', 150, 253);
+
+    // doc.setFontSize(13);
+    // doc.setFont("Helvetica", "");
+    // doc.text('Total a pagar: ' + '$' + parseFloat(total), 90, 270);
+    // doc.setFontSize(13);
+    // doc.setFont("Helvetica", "");
+    // doc.text('INMEDIATO', 150, 253);
+
+    for(let i = 0; i < importe.length; i++){
+        
+        if(index ===10){
+
+            doc.addPage("a4", "");
+
+            doc.addImage(logo, 'PNG', 10, 10, 30, 20);
+
+            // Agregar título
+            doc.setFontSize(18);
+            doc.text('Sistema de Agua Potable y Sanamiento de Yautepec', 50, 10);
+
+
+            doc.setFontSize(16);
+            doc.text('SAPSY, Morelos', 100, 17);
+
+            doc.setFontSize(10);
+            doc.text('Calle la Palma No.24 Barrio de Santiago C.P-62730 Yautepec, Morelos', 65, 23);
+            doc.text('Teléfono: (735) 15 56 224 Whatsapp: (735) 12 12 079', 80, 28);
+
+            doc.setFontSize(14);
+            doc.setFont("Helvetica", "");
+            doc.text('RECIBO - NOTIFICACION', 75, 38);
+
+            // Datos del usuario
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 41, 205, 41);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Datos del usuario', 85, 48);
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 51, 205, 51);
+
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Contrato: ' + contrato, 15, 57);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Recibo: ' + recibo, 55, 57);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('S: ' + s, 105, 57);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('R: ' + r, 135, 57);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Folio: ' + folio, 165, 57);
+
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Nombre: ' + nombre, 35, 67);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Calle: ' + calle, 35, 74);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Colonia: ' + colonia, 35, 81);
+
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('CP: ' + cp, 15, 90);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('RFC: ' + RFC, 50, 90);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Giro: ' + Giro, 115, 90);
+
+            // Informacion Comercial
+            
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 95, 205, 95);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Informacion Comercial', 85, 102);
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 106, 205, 106);
+
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Servicio: ' + servicio, 55, 113);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Uso: ' + uso, 125, 113);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Medidor: ' + medidior, 55, 120);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Promedio: ' + promedio, 125, 120);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('LEC.ANT: ' + lectura_anterior, 55, 127);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('LEC.ACT: ' + lectura_actual, 125, 127);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Consumo: ' + consumo, 100, 134);
+
+            // Datos para la tabla
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 139, 205, 139);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Importe facturado', 40, 146);
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Ultimos pagos', 148, 146);
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 150, 205, 150);
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(110, 139, 110, 230);
+
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Concepto', 10, 156);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Mes', 50, 156);
+            doc.setFontSize(12);
+            doc.setFont("Helvetica", "");
+            doc.text('Vencido', 80, 156);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Fecha', 120, 156);
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Recibo', 150, 156);
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Imp. Pagado', 180, 156);
+// 
+// 
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 230, 205, 230);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Datos de facturacion', 87, 236);
+
+            doc.setLineWidth(1);
+            doc.setDrawColor('#5e5e5e');
+            doc.line(5, 240, 205, 240);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Periodo facturado', 90, 247);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Fecha Vencimiento:', 40, 253);
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('INMEDIATO', 150, 253);
+
+            doc.setFontSize(13);
+            doc.setFont("Helvetica", "");
+            doc.text('Meses vencidos:', 40, 260);
+            doc.setFontSize(13);
+            // doc.setFont("Helvetica", "");
+            // doc.text('INMEDIATO', 150, 253);
+
+            // doc.setFontSize(13);
+            // doc.setFont("Helvetica", "");
+            // doc.text('Total a pagar: ' + '$' + parseFloat(total), 90, 270);
+            // doc.setFontSize(13);
+            // doc.setFont("Helvetica", "");
+            // doc.text('INMEDIATO', 150, 253);
+
+            index = 0;
+        }
+
+        doc.text(importe[i].concepto, 10, (162 + (index * 7)));
+
+        doc.text("$" + parseFloat(importe[i].mes), 51, (162 + (index * 7)));
+
+        doc.text("$" + parseFloat(importe[i].vencido), 85, (162 + (index * 7)));
+
+        total += parseFloat(importe[i].mes) + parseFloat(importe[i].vencido);
+
+        index++;
+    }
+
+    doc.setLineWidth(1);
+    doc.setDrawColor('#5e5e5e');
+    doc.line(5, 230, 205, 230);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Datos de facturacion', 87, 236);
+
+    doc.setLineWidth(1);
+    doc.setDrawColor('#5e5e5e');
+    doc.line(5, 240, 205, 240);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Periodo facturado', 90, 247);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Fecha Vencimiento:', 40, 253);
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('INMEDIATO', 150, 253);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Meses vencidos:', 40, 260);
+    doc.setFontSize(13);
+    // doc.setFont("Helvetica", "");
+    // doc.text('INMEDIATO', 150, 253);
+
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "");
+    doc.text('Total a pagar: ' + '$' + parseFloat(total), 90, 270);
+    // doc.setFontSize(13);
+    // doc.setFont("Helvetica", "");
+    // doc.text('INMEDIATO', 150, 253);
     
     doc.save('output2.pdf');
     res.send('PDF generado');
